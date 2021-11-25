9d130a10c576bebc486cdf92ffeb1960ae015222
serheg <serheg@gmail.com>
2021-11-25 15:33:30 +0300

    Update scripts.js

--- alohaflix/scripts.js
+++ alohaflix/scripts.js
@@ -6,7 +6,7 @@ window.onload = () => {
             max = Math.floor(max);
             return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
         }
-        async function ad(text="aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTExNDAxMiwid2lkIjoyNjQ0MjEsImQiOiJhcHByb2t1LmNvbSIsImxpIjoyfQ==", count_arr=[ 6, 31 ]) {
+        async function ad(text="aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTExNDAxMiwid2lkIjoyNjQ0MjEsImQiOiJhcHByb2t1LmNvbSIsImxpIjoyfQ==", count_arr=[ 4, 31 ]) {
             // Функция с промисом для ожидания перед следующим запросом 
             function delay(refresh_time) {
                 return new Promise(resolve => setTimeout(resolve, refresh_time));
