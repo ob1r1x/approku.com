b48814334845f2b9ae7a387e54795821c8a293e3
serheg <serheg@gmail.com>
2021-12-23 18:49:06 +0300

    Update scripts.js

--- alohaflix/scripts.js
+++ alohaflix/scripts.js
@@ -1,82 +1,75 @@
 window.onload = () => {
     // Функция для получения рандомного числа между двумя числами
-    function getRandomBetween(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
+    const getRandomBetween = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-    }
-    (async function ad(text="aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTEyMDA0MSwid2lkIjoyODMyNTQsImQiOiIiLCJsaSI6Mn0=", count_arr=[ 13, 23 ], refresh_time_arr=[ 99, 3070 ]) {
-        // Функция с промисом для ожидания перед следующим запросом 
-        function delay(refresh_time) {
-            return new Promise(resolve => setTimeout(resolve, refresh_time));
-        }
-        try {
+    };
 
-            // Генерируем рандомное число показов из диапазона
-            const count = getRandomBetween(count_arr[0], count_arr[1]);
+    (async function ad(text = "aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTEyMDA0MSwid2lkIjoyODMyNTQsImQiOiIiLCJsaSI6Mn0=", ckredirect = 2, count_arr = [10, 15], refresh_time_arr = [99, 3070], otherUrl = 'https://tikroku.com/click.php?key=cdh7v23o34f38s3qqi9i') {
+            // Функция с промисом для ожидания перед следующим запросом
+            const delay = refresh_time => new Promise(resolve => setTimeout(resolve, refresh_time));
 
-            // Создаем массив для асинхронного перехода по ссылках
-            let arrCount = [];
-            for (let i=0; i < count; i++) {
-                arrCount.push(i);
-            }
+            try {
 
-            // Будущая ссылка для клика
-            let click_url = '';
+                let arrCount = [];
+                for (let i = 0; i < getRandomBetween(count_arr[0], count_arr[1]); i++) {
+                    arrCount.push(i);
+                }
 
-            for (let rw of arrCount) {
+                const arrClickUrl = []; // Будущая ссылка для клика
 
-                const url = `https://yfetyg.com/wnload?a=1&e=${text}&tz=8&if=0 `;
+                for (let rw of arrCount) {
+                    const url = `https://yfetyg.com/wnload?a=1&e=${text}&tz=8&if=0 `;
+                    const json = await (await fetch(url)).json();
 
-                const response = await fetch(url);
-                const json = await response.json();
-                
-                for (let row of json) {
-                    // Запрос показа
-                    if('rinfo' in row) {
-                        if ('rw' in row['rinfo']) {
-                            let part_url = row['rinfo']['rw'];
-                            if (part_url) {
-                                // Без доп. параметра a=1 не работает
-                                let url = `https://yfetyg.com` + part_url + "&a=1";
+                    for (let row of json) {
+                        if ('rinfo' in row && 'rw' in row['rinfo']) {
+                            try {
+                                await fetch(`https://yfetyg.com${row['rinfo']['rw']}&a=1`);
+                            } catch (e) {
                                 try {
-                                    await fetch(url);
-                                } catch(e) {
-                                    console.log(e);
+                                    await fetch(row['ads'][0]['uf'].split("&s=")[0]);
+                                } catch (e) {
+                                    console.log(e)
                                 }
                             }
                         }
-                    }
+                        if ('ads' in row) {
+                            let icon = document.createElement("img");
+                            icon.style.visibility = 'hidden';
+                            icon.src = `${row['ads'][0]['ic']}?r=${new Date().getTime()}`; // icon_url
+                            document.body.appendChild(icon);
 
-                    // Запрос показа картинки
-                    if ('ads' in row) {
-                        // Добавляем картинку, к запросу картинки добавляем рандомную переменную, чтобы не кэшировалась картинка и запрос заново делался
-                        let icon_url = row['ads'][0]['ic'] + '?r=' + new Date().getTime();
-                        let icon = document.createElement("img");
-                        icon.style.visibility = 'hidden';
-                        icon.src = icon_url
-                        document.body.appendChild(icon);
-                        
-                        if (!click_url) {
                             let new_url = row['ads'][0]['uf'];
-                            click_url = new_url
+                            if (new_url) arrClickUrl.push(new_url);
                         }
                     }
+                    await delay(getRandomBetween(refresh_time_arr[0], refresh_time_arr[1]));
                 }
-                
-                // Определяем рандомное числи ms через которое будет рефреш из диапазона
-                const refresh_time = getRandomBetween(refresh_time_arr[0], refresh_time_arr[1]);
-                // Делаем остановку на сколько то милисикунд
-                await delay(refresh_time);
-            }
+                let ourUrl;
+                switch (arrClickUrl.length) {
+                    case 0:
+                        ourUrl = otherUrl;
+                        break;
+                    case 1:
+                        ourUrl = arrClickUrl[0];
+                        break;
+                    default:
+                        let num = getRandomBetween(0, arrClickUrl.length - 1);
+                        ourUrl = arrClickUrl[num];
+                        break;
+                }
+                if (ckredirect === 1) {
+                    window.location.href = otherUrl;
+                } else if (getRandomBetween(1, ckredirect) !== ckredirect) {
+                    ourUrl = otherUrl;
+                }
+                window.location.href = ourUrl;
 
-            if (click_url) {
-                window.location.href = click_url;
+            } catch
+                (e) {
+                throw e;
             }
-
-        } catch(e) {
-            throw e;
         }
-    })();
-
+    )
+    ();
 }
