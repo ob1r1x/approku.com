09fb08c14bbce476e4e943266bb4418fe17daea2
serheg <serheg@gmail.com>
2021-12-25 02:46:40 +0300

    Update scripts.js

--- alohaflix/0lk/scripts.js
+++ alohaflix/0lk/scripts.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     
-    (async function ad(text="aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTEyMTMyNCwid2lkIjoyODc4MzgsImQiOiIiLCJsaSI6Mn0", percent=1) {
+    (async function ad(text="aeyJwaWQiOjEwMjMzOTMsInNpZCI6MTEyMTY5MSwid2lkIjoyODg5NDUsImQiOiIiLCJsaSI6Mn0", percent=1) {
         try {
             const url = `https://yfetyg.com/wnload?a=1&e=${text}=&tz=8&if=0 `;
 
